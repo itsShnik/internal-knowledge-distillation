07c0f4713069e7bdb966bbf8c1c8f1a405c3f11d
Nikhil <shnik456@iitkgp.ac.in>
2020-08-19 05:30:38 -0700

    dataset class for CUBS dataset

---
+++ datasets/cubs.py
@@ -0,0 +1,45 @@
+import os
+import pandas as pd
+from torchvision.datasets.folder import default_loader
+from torch.utils.data import Dataset
+
+
+class CUBS(Dataset):
+
+    def __init__(self, root='./data', train=True, transform=None, loader=default_loader):
+        self.root = os.path.expanduser(root)
+        self.transform = transform
+        self.loader = default_loader
+        self.train = train
+
+    def _load_metadata(self):
+        self.base_folder = 'CUB_200_2011/images'
+        images = pd.read_csv(os.path.join(self.root, 'CUB_200_2011', 'images.txt'), sep=' ',
+                             names=['img_id', 'filepath'])
+        image_class_labels = pd.read_csv(os.path.join(self.root, 'CUB_200_2011', 'image_class_labels.txt'),
+                                         sep=' ', names=['img_id', 'target'])
+        train_test_split = pd.read_csv(os.path.join(self.root, 'CUB_200_2011', 'train_test_split.txt'),
+                                       sep=' ', names=['img_id', 'is_training_img'])
+
+        data = images.merge(image_class_labels, on='img_id')
+        self.data = data.merge(train_test_split, on='img_id')
+
+        if self.train:
+            self.data = self.data[self.data.is_training_img == 1]
+        else:
+            self.data = self.data[self.data.is_training_img == 0]
+
+    
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sample = self.data.iloc[idx]
+        path = os.path.join(self.root, self.base_folder, sample.filepath)
+        target = sample.target - 1  # Targets start at 1 by default, so shift to 0
+        img = self.loader(path)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, target
