208478405f8235aec6de69b01c267794f5cb08dd
Nikhil <shnik456@iitkgp.ac.in>
2020-08-24 22:52:14 -0700

    calling batch end and epoch end callbacks; train and val metrics update

--- common/trainer.py
+++ common/trainer.py
@@ -10,6 +10,31 @@ from collections import namedtuple
 #----------------------------------------
 import torch
 
+# Parameter to pass to batch_end_callback
+BatchEndParam = namedtuple('BatchEndParams',
+                           ['epoch',
+                            'nbatch',
+                            'rank',
+                            'add_step',
+                            'data_in_time',
+                            'data_transfer_time',
+                            'forward_time',
+                            'backward_time',
+                            'optimizer_time',
+                            'metric_time'])
+
+def _multiple_callbacks(callbacks, *args, **kwargs):
+    """Sends args and kwargs to any configured callbacks.
+    This handles the cases where the 'callbacks' variable
+    is ``None``, a single function, or a list.
+    """
+    if isinstance(callbacks, list):
+        for cb in callbacks:
+            cb(*args, **kwargs)
+        return
+    if callbacks:
+        callbacks(*args, **kwargs)
+
 def to_cuda(batch):
     # convert batch: tuple to batch: list
     batch = list(batch)
@@ -23,8 +48,14 @@ def to_cuda(batch):
 def train(config,
         net,
         optimizer,
+        train_loader,
+        train_metrics,
+        val_loader,
+        val_metrics,
         lr_scheduler=None,
-        rank=None):
+        rank=None,
+        batch_end_callbacks=None,
+        epoch_end_callbacks=None):
 
     for epoch in range(config.TRAIN.BEGIN_EPOCH, config.TRAIN.END_EPOCH):
 
@@ -37,7 +68,7 @@ def train(config,
         end_time = time.time()
 
         # start training
-        for nbatch, batch in enumerate(train_loader):
+        for nbatch, (images, labels) in enumerate(train_loader):
             global_steps = len(train_loader) * epoch + nbatch
 
             # record time
@@ -47,17 +78,22 @@ def train(config,
             data_transfer_time = time.time()
             batch = to_cuda(batch)
             data_transfer_time = time.time() - data_transfer_time
+
             # clear the gradients
             optimizer.zero_grad()
 
             # forward time
             forward_time = time.time()
-
-            # forward pass
-            outputs, loss = net(*batch)
-
-            # forward_time
+            outputs = net(images)
             forward_time = time.time() - forward_time
+
+            # calculate losses
+            loss = criterion(outputs, labels)
+
+            # update training metrics
+            metric_time = time.time()
+            train_metrics.update(outputs, labels, loss.clone().detach())
+            metric_time = time.time() - metric_time
 
             # backward time
             backward_time = time.time()
@@ -73,9 +109,28 @@ def train(config,
             optimizer.step()
             optimizer_time = time.time() - optimizer_time
 
-            #TODO: batch end callbacks, once configured
+            # execute batch_end_callbacks
+            if batch_end_callbacks is not None:
+                batch_end_params = BatchEndParam(epoch=epoch, nbatch=nbatch, add_step=True, rank=rank,
+                                                 data_in_time=data_in_time, data_transfer_time=data_transfer_time,
+                                                 forward_time=forward_time, backward_time=backward_time,
+                                                 optimizer_time=optimizer_time, metric_time=metric_time
+                                                 )
+                _multiple_callbacks(batch_end_callbacks, batch_end_params)
+
+            # Print accuracy and loss
+            metrics = train_metrics.get()
+            print(f'[Rank: {0 if rank is None else rank}] Training Accuracy: {metrics["training_accuracy"]} Loss: {metrics["training_loss"]}')
 
             # update end time
             end_time = time.time()
 
-        #TODO: execute epoch_end_callbacks, once configured
+        # First do validation at the end of each epoch
+        val_acc = do_validation(net, val_loader)
+
+        # update validation metrics
+        val_metrics.update(epoch, val_acc)
+
+        if epoch_end_callbacks is not None:
+            _multiple_callbacks(epoch_end_callbacks, epoch, net, optimizer)
+
