5ffa29f010a37b6873b69fc162797dbc4a360cd3
Nikhil <shnik456@iitkgp.ac.in>
2020-08-31 03:06:47 -0700

    removed filter from optimizer

--- common/utils.py
+++ common/utils.py
@@ -6,7 +6,7 @@ import torch
 def optim_SGD(model, initial_lr=0.1, momentum=0.9, weight_decay=0.05):
 
     # return an SGD optimizer object
-    return torch.optim.SGD(filter(lambda f: f.requires_grad, model.parameters()), lr=initial_lr, momentum=momentum, weight_decay=weight_decay)
+    return torch.optim.SGD(model.parameters(), lr=initial_lr, momentum=momentum, weight_decay=weight_decay)
 
 def lr_scheduler_StepLR(optimizer, step_size=30, gamma=0.1):
 
