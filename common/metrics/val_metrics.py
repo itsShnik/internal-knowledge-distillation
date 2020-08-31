423fed241c64c3e7040c8f8a81c12cfacec0fcd5
Nikhil <shnik456@iitkgp.ac.in>
2020-08-31 04:22:02 -0700

    using floating pt values now and commented multi gpu part'

--- common/metrics/val_metrics.py
+++ common/metrics/val_metrics.py
@@ -18,21 +18,21 @@ class ValMetrics():
         self.current_epoch = epoch
 
         # convert val_acc to tensor
-        val_acc = torch.tensor(val_acc)
+        # val_acc = torch.tensor(val_acc)
 
-        if self.allreduce:
-            val_acc = val_acc.clone().cuda()
-            gpus = torch.tensor(1.).cuda()
+        #if self.allreduce:
+        #    val_acc = val_acc.clone().cuda()
+        #    gpus = torch.tensor(1.).cuda()
 
-            # Sum validation accuracies across GPUs
-            distributed.all_reduce(val_acc, op=distributed.ReduceOp.SUM)
-            distributed.all_reduce(gpus, op=distributed.ReduceOp.SUM)
+        #    # Sum validation accuracies across GPUs
+        #    distributed.all_reduce(val_acc, op=distributed.ReduceOp.SUM)
+        #    distributed.all_reduce(gpus, op=distributed.ReduceOp.SUM)
 
-            val_acc = (val_acc / gpus).cpu()
+        #    val_acc = (val_acc / gpus).cpu()
 
 
         # current validation accuracy
-        self.current_val_acc = val_acc.item()
+        self.current_val_acc = val_acc
 
         # compare with best val acc
         if self.current_val_acc > self.best_val_acc:
