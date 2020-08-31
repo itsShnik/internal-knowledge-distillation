571bc2a3c506d23a69fd3591b52071f9b7900691
Nikhil <shnik456@iitkgp.ac.in>
2020-08-31 04:21:42 -0700

    using floating pt values now and commented multi gpu part'

--- common/metrics/train_metrics.py
+++ common/metrics/train_metrics.py
@@ -6,43 +6,45 @@ import torch.distributed as distributed
 
 class TrainMetrics():
     def __init__(self, config, allreduce=False):
-        self.correct_instances = torch.tensor(0.)
-        self.total_instances = torch.tensor(0.)
-        self.training_accuracy = torch.tensor(0.)
-        self.batch_loss = torch.tensor(0.)
         self.allreduce = allreduce
+        self.reset()
 
     def reset(self):
         # To reseet the metrics after every epoch
-        self.correct_instances = torch.tensor(0.)
-        self.total_instances = torch.tensor(0.)
-        self.training_accuracy = torch.tensor(0.)
-        self.batch_loss = torch.tensor(0.)
+        self.correct_instances = 0
+        self.total_instances = 0
+        self.average_training_accuracy = 0.
+        self.batch_accuracy = 0.
+        self.batch_loss = 0.
 
     def update(self, outputs, labels, loss):
         # calculate the number of correct instances
         predicted = torch.argmax(outputs.data, 1)
-        self.total_instances += labels.size(0)
-        self.correct_instances += (predicted == labels).sum().item()
+        correct_instances = (predicted == labels).sum().cpu().item()
+        new_instances = labels.size(0)
+
+        # update total instances
+        self.total_instances += new_instances
+        self.correct_instances += correct_instances
 
         # share this data across GPUs
-        if self.allreduce:
-            total_instances = self.total_instances.clone().cuda()
-            correct_instances = self.correct_instances.clone().cuda()
+        #if self.allreduce:
+        #    total_instances = self.total_instances.clone().cuda()
+        #    correct_instances = self.correct_instances.clone().cuda()
 
-            # sum across GPUs
-            distributed.all_reduce(total_instances, op=distributed.ReduceOp.SUM)
-            distributed.all_reduce(correct_instances, op=distributed.ReduceOp.SUM)
+        #    # sum across GPUs
+        #    distributed.all_reduce(total_instances, op=distributed.ReduceOp.SUM)
+        #    distributed.all_reduce(correct_instances, op=distributed.ReduceOp.SUM)
 
-            self.training_accuracy = 100.0 * (correct_instances / total_instances).detach().cpu()
+        #    self.training_accuracy = 100.0 * (correct_instances / total_instances).detach().cpu()
 
-        else:
-            self.training_accuracy = 100.0 * (self.correct_instances / self.total_instances).detach().cpu()
-
+        #else:
+        self.batch_accuracy = 100.0 * (correct_instances / new_instances)
+        self.training_accuracy = 100.0 * (self.correct_instances / self.total_instances)
         self.batch_loss = loss
 
     def get(self):
-        return {'training_loss':self.batch_loss, 'training_accuracy':self.training_accuracy}
+        return {'training_loss':self.batch_loss, 'batch_accuracy':self.batch_accuracy, 'training_accuracy':self.training_accuracy}
 
 
 
