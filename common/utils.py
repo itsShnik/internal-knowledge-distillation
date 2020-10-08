d4e713dfa821bd391eb9bbca5443faee86a0e2ec
Nikhil <shnik456@iitkgp.ac.in>
2020-10-08 02:35:55 -0700

    added a model loading function

--- common/utils.py
+++ common/utils.py
@@ -42,6 +42,24 @@ def standard_model_load(model, pretrain_state_dict):
     new_state_dict.update(parsed_state_dict)
     model.load_state_dict(new_state_dict)
 
+def standard_with_classifier_model_load(model, pretrain_state_dict):
+
+    # parse from multiple gpu to single or vice versa
+    parsed_state_dict = {}
+    for k, v in pretrain_state_dict.items():
+        if k not in model.state_dict():
+            if k.startswith('module.'):
+                k = k[len('module.'):]
+            else:
+                k = 'module.' + k
+        if k in model.state_dict():
+            parsed_state_dict[k] = v
+
+    # Now load this state dict to our model
+    new_state_dict = model.state_dict()
+    new_state_dict.update(parsed_state_dict)
+    model.load_state_dict(new_state_dict)
+
 def parallel_model_load(model, pretrain_state_dict):
 
     # parse from multiple gpu to single or vice versa
