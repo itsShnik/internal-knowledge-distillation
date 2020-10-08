a1f7bfdc825109c013846e96235acc7e7205680b
Nikhil <shnik456@iitkgp.ac.in>
2020-10-07 23:56:26 -0700

    added option for sgd nesterov momentum optimizer

--- common/optim.py
+++ common/optim.py
@@ -12,6 +12,11 @@ def optim_SGD(model=None, initial_lr=0.1, momentum=0.9, weight_decay=0.05, **kwa
     # return an SGD optimizer object
     return torch.optim.SGD(model.parameters(), lr=initial_lr, momentum=momentum, weight_decay=weight_decay)
 
+def optim_SGD_Nesterov(model=None, initial_lr=0.1, momentum=0.9, weight_decay=0.05, **kwargs):
+
+    # return an SGD optimizer object
+    return torch.optim.SGD(model.parameters(), lr=initial_lr, momentum=momentum, weight_decay=weight_decay, nesterov=True)
+
 def optim_AdamW(model=None, initial_lr=0.1, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.05, **kwargs):
 
     # return AdamW object
