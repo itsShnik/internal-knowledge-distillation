e9429472500843ce47a63f0c50d7ae4ad5f80249
Nikhil <shnik456@iitkgp.ac.in>
2020-10-25 04:28:40 -0700

    added the frozen teacher in kd loss functions

--- common/losses.py
+++ common/losses.py
@@ -17,3 +17,18 @@ def loss_fn_kd(student_outputs, teacher_outputs, alpha=0.5, temp=10):
                             F.softmax(teacher_outputs/temp, dim=1)) * (alpha * temp * temp)
 
     return kd_loss
+
+def loss_fn_kd_frozen_teacher(student_outputs, teacher_outputs, alpha=0.5, temp=10):
+    """
+    Compute the knowledge-distillation (KD) loss given outputs, labels.
+    "Hyperparameters": temperature and alpha
+
+    NOTE: the KL Divergence for PyTorch comparing the softmaxs of teacher
+    and student expects the input tensor to be log probabilities! See Issue #2
+    """
+    teacher_outputs_clone = teacher_outputs.clone().detach()
+
+    kd_loss = nn.KLDivLoss()(F.log_softmax(student_outputs/temp, dim=1),
+                            F.softmax(teacher_outputs_clone/temp, dim=1)) * (alpha * temp * temp)
+
+    return kd_loss
