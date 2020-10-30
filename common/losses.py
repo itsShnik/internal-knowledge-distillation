ac1d86010ffcf79fe714090606df0684b63f01d3
Nikhil <shnik456@iitkgp.ac.in>
2020-10-30 03:23:31 -0700

    loss and batch accuracy are calculated here

--- common/losses.py
+++ common/losses.py
@@ -1,10 +1,23 @@
 #----------------------------------------
 #--------- Torch related imports --------
 #----------------------------------------
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def loss_fn_kd(student_outputs, teacher_outputs, alpha=0.5, temp=10):
+def calculate_loss_and_accuracy(criterion, outputs, labels):
+
+    loss = criterion(outputs, labels)
+
+    # calculate the accuracy here
+    predicted = torch.argmax(outputs.data, 1)
+    correct_instances = (predicted == labels).sum().item() 
+    total_instances = labels.size(0)
+    accuracy = 100.0 * (correct_instances / total_instances)
+
+    return loss, accuracy
+
+def loss_fn_kd(student_outputs, teacher_outputs, alpha, temp):
     """
     Compute the knowledge-distillation (KD) loss given outputs, labels.
     "Hyperparameters": temperature and alpha
@@ -14,11 +27,11 @@ def loss_fn_kd(student_outputs, teacher_outputs, alpha=0.5, temp=10):
     """
 
     kd_loss = nn.KLDivLoss()(F.log_softmax(student_outputs/temp, dim=1),
-                            F.softmax(teacher_outputs/temp, dim=1)) * (alpha * temp * temp)
+                            F.softmax(teacher_outputs/temp, dim=1))
 
     return kd_loss
 
-def loss_fn_kd_frozen_teacher(student_outputs, teacher_outputs, alpha=0.5, temp=10):
+def loss_fn_kd_frozen_teacher(student_outputs, teacher_outputs, alpha, temp):
     """
     Compute the knowledge-distillation (KD) loss given outputs, labels.
     "Hyperparameters": temperature and alpha
@@ -29,6 +42,6 @@ def loss_fn_kd_frozen_teacher(student_outputs, teacher_outputs, alpha=0.5, temp=
     teacher_outputs_clone = teacher_outputs.clone().detach()
 
     kd_loss = nn.KLDivLoss()(F.log_softmax(student_outputs/temp, dim=1),
-                            F.softmax(teacher_outputs_clone/temp, dim=1)) * (alpha * temp * temp)
+                            F.softmax(teacher_outputs_clone/temp, dim=1))
 
     return kd_loss
