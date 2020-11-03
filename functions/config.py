256ebfb22c07a2fb88b3956199f0ab4b9a96684f
Nikhil <shnik456@iitkgp.ac.in>
2020-11-02 19:31:08 -0800

    added another hyperparameter beta

--- functions/config.py
+++ functions/config.py
@@ -20,6 +20,7 @@ _C.POLICY_MODULE = 'resnet8'
 _C.USE_KD_LOSS = False
 _C.ALPHA = 0.5
 _C.TEMPERATURE = 10
+_C.BETA = 1.0
 _C.KD_LOSS_FUNCTION = 'loss_fn_kd'
 _C.PROJECT = 'internal-knowledge-distillation'
 
