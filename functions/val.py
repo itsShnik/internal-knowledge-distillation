9ac3f5a840e4816d680a5bb11ba9643f7bc46fd9
Nikhil <shnik456@iitkgp.ac.in>
2020-11-02 19:30:13 -0800

    added the additional stochastic strategy in val functions

--- functions/val.py
+++ functions/val.py
@@ -17,7 +17,7 @@ def do_validation(config, net, val_loader, policy_net=None):
     correct_instances = 0
     total_instances = 0
 
-    if config.NETWORK.TRAINING_STRATEGY == 'AdditionalHeads':
+    if config.NETWORK.TRAINING_STRATEGY in ['AdditionalHeads', 'AdditionalStochastic']:
         additional_correct_instances = [0] * config.NETWORK.NUM_ADDITIONAL_HEADS
         additional_total_instances = [0] * config.NETWORK.NUM_ADDITIONAL_HEADS
 
@@ -30,10 +30,10 @@ def do_validation(config, net, val_loader, policy_net=None):
             policy_action = gumbel_softmax(policy_vector.view(policy_vector.size(0), -1, 2))
             policy = policy_action[:,:,1]
             outputs = net(images, policy)
-        elif config.NETWORK.TRAINING_STRATEGY == 'AdditionalHeads':
-            outputs, additional_outputs = net(images)
+        elif config.NETWORK.TRAINING_STRATEGY in ['AdditionalHeads', 'AdditionalStochastic']:
+            outputs, additional_outputs = net(images, mode='val')
         else:
-            outputs = net(images)
+            outputs = net(images, mode='val')
 
         # calculate the accuracy
         predicted = torch.argmax(outputs.data, 1)
@@ -41,7 +41,7 @@ def do_validation(config, net, val_loader, policy_net=None):
         correct_instances += (predicted == labels).sum().item()
 
         # calculate additional accuracy if have to 
-        if config.NETWORK.TRAINING_STRATEGY == 'AdditionalHeads':
+        if config.NETWORK.TRAINING_STRATEGY in ['AdditionalHeads', 'AdditionalStochastic']:
             for i in range(config.NETWORK.NUM_ADDITIONAL_HEADS):
                 additional_predicted = torch.argmax(additional_outputs[i].data, 1)
                 additional_total_instances[i] += labels.size(0)
@@ -51,7 +51,7 @@ def do_validation(config, net, val_loader, policy_net=None):
     val_acc = (100.0 * correct_instances) / total_instances
 
     # additional val acc
-    if config.NETWORK.TRAINING_STRATEGY == 'AdditionalHeads':
+    if config.NETWORK.TRAINING_STRATEGY in ['AdditionalHeads', 'AdditionalStochastic']:
         return_list = []
         for i in range(config.NETWORK.NUM_ADDITIONAL_HEADS):
             additional_val_acc = (100.0 * additional_correct_instances[i]) / additional_total_instances[i]
