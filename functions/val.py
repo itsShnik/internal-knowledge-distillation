ec2860af204870219e994b8656fca6b206b50bd1
Nikhil <shnik456@iitkgp.ac.in>
2020-12-22 02:12:58 -0800

    removed strategy specific val, sam val in all cases

--- functions/val.py
+++ functions/val.py
@@ -17,9 +17,8 @@ def do_validation(config, net, val_loader, policy_net=None):
     correct_instances = 0
     total_instances = 0
 
-    if config.NETWORK.TRAINING_STRATEGY in ['AdditionalHeads', 'AdditionalStochastic']:
-        additional_correct_instances = [0] * config.NETWORK.NUM_ADDITIONAL_HEADS
-        additional_total_instances = [0] * config.NETWORK.NUM_ADDITIONAL_HEADS
+    additional_correct_instances = [0] * config.NETWORK.NUM_ADDITIONAL_HEADS
+    additional_total_instances = [0] * config.NETWORK.NUM_ADDITIONAL_HEADS
 
     for nbatch, batch in enumerate(val_loader):
         images, labels = to_cuda(batch)
@@ -30,10 +29,8 @@ def do_validation(config, net, val_loader, policy_net=None):
             policy_action = gumbel_softmax(policy_vector.view(policy_vector.size(0), -1, 2))
             policy = policy_action[:,:,1]
             outputs = net(images, policy)
-        elif config.NETWORK.TRAINING_STRATEGY in ['AdditionalHeads', 'AdditionalStochastic']:
-            outputs, additional_outputs = net(images, mode='val')
         else:
-            outputs = net(images, mode='val')
+            outputs, additional_outputs = net(images, mode='val')
 
         # calculate the accuracy
         predicted = torch.argmax(outputs.data, 1)
@@ -41,21 +38,17 @@ def do_validation(config, net, val_loader, policy_net=None):
         correct_instances += (predicted == labels).sum().item()
 
         # calculate additional accuracy if have to 
-        if config.NETWORK.TRAINING_STRATEGY in ['AdditionalHeads', 'AdditionalStochastic']:
-            for i in range(config.NETWORK.NUM_ADDITIONAL_HEADS):
-                additional_predicted = torch.argmax(additional_outputs[i].data, 1)
-                additional_total_instances[i] += labels.size(0)
-                additional_correct_instances[i] += (additional_predicted == labels).sum().item()
+        for i in range(config.NETWORK.NUM_ADDITIONAL_HEADS):
+            additional_predicted = torch.argmax(additional_outputs[i].data, 1)
+            additional_total_instances[i] += labels.size(0)
+            additional_correct_instances[i] += (additional_predicted == labels).sum().item()
 
     # return accuracy
     val_acc = (100.0 * correct_instances) / total_instances
 
     # additional val acc
-    if config.NETWORK.TRAINING_STRATEGY in ['AdditionalHeads', 'AdditionalStochastic']:
-        return_list = []
-        for i in range(config.NETWORK.NUM_ADDITIONAL_HEADS):
-            additional_val_acc = (100.0 * additional_correct_instances[i]) / additional_total_instances[i]
-            return_list.append(additional_val_acc)
-        return val_acc, return_list
-
-    return val_acc
+    return_list = []
+    for i in range(config.NETWORK.NUM_ADDITIONAL_HEADS):
+        additional_val_acc = (100.0 * additional_correct_instances[i]) / additional_total_instances[i]
+        return_list.append(additional_val_acc)
+    return val_acc, return_list
